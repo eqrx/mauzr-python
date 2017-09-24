ec37ff1b7600747dbbd3cf065d5d14c4c9886c94
Alexander Sowitzki <dev@eqrx.net>
2017-09-24 20:30:29 +0200

    Add cpython launcher

--- mauzr/__init__.py
+++ mauzr/__init__.py
@@ -1,6 +1,24 @@
 """ Factories for platform specific core implementations. """
 __author__ = "Alexander Sowitzki"
 
+def cpython(suit, agent, instance=None, parser=None):
+    """ Create core for cpython.
+
+    :param suit: Suit this agent belongs to.
+    :type suit: str
+    :param agent: Name of the agent.
+    :type agent: str
+    :param instance: Instance of this agent. May be None
+    :type instance: str
+    :param parser: Argparse instance to use. If None, a new one will be used.
+    :type parser: argparse.ArgumentParser
+    :returns: Core instance.
+    :rtype: mauzr.platform.linux.Core
+    """
+
+    from mauzr.platform.cpython import Core
+    return Core(suit, agent, instance, parser)
+
 def linux(suit, agent, instance=None, parser=None):
     """ Create core for linux.
 
